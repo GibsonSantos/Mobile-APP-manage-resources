8dd9f46ac0c7e6650c3b14a55c2de314d3101cad
Gibson Oliveira dos Santos Filho <98161396+GibsonSantos@users.noreply.github.com>
2023-01-11 10:16:45 +0000

    hello world

--- app/src/main/java/com/example/manageresourceshome/MainActivity.java
+++ app/src/main/java/com/example/manageresourceshome/MainActivity.java
@@ -2,15 +2,26 @@ package com.example.manageresourceshome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int reqCode = 1; // requestCode
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int x = 0;
+
+    }
+
+    //passando o utilizador para a activit novo jogo
+    public void btnStart(View view){
+        // creates the explicit intent
+        Intent intent = new Intent(getApplicationContext(), Menu.class);
+        startActivityForResult(intent, reqCode);
     }
 }
