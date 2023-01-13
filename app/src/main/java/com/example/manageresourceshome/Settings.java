24925f97ead5edaae098f39bdf2f28056f33ec80
Gibson Oliveira dos Santos Filho <98161396+GibsonSantos@users.noreply.github.com>
2023-01-13 19:18:47 +0000

    update

--- app/src/main/java/com/example/manageresourceshome/Settings.java
+++ app/src/main/java/com/example/manageresourceshome/Settings.java
@@ -1,19 +1,91 @@
 package com.example.manageresourceshome;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Settings extends AppCompatActivity {
 
     private int reqCode = 1; // requestCode
+    private Button btn_reset_days;
+    private Button btn_reset_months;
+    private appDBAdapter gAdapter;
+    private AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        gAdapter = new appDBAdapter(this);
+        builder = new AlertDialog.Builder(this);
+
+        btn_reset_days = (Button) findViewById(R.id.btn_reset_data_days);
+        btn_reset_months = (Button) findViewById(R.id.btn_reset_data_month);
+
+        btn_reset_days.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                builder.setMessage("Deseja mesmo apagar os dados armazenados de consumo diário?")
+                        .setCancelable(false)
+                        .setPositiveButton("SIM", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                gAdapter.open();
+                                gAdapter.clearDataMount();
+                                gAdapter.close();
+                                Toast.makeText(getApplicationContext(),"Dados apagados!",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("NÃO", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                //cancela o dialogo caso o utilizador deseje continuar no jogo
+                                dialog.cancel();
+                            }
+                        });
+                //Cria um caixa de dialogo
+                AlertDialog alert = builder.create();
+                //Setting the title manually
+                alert.setTitle("ALERTA");
+                alert.show();
+            }
+        });
+
+        btn_reset_months.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                builder.setMessage("Deseja mesmo apagar os dados armazenados de consumo por mês?")
+                        .setCancelable(false)
+                        .setPositiveButton("SIM", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                gAdapter.open();
+                                gAdapter.clearDataYear();
+                                gAdapter.close();
+                                Toast.makeText(getApplicationContext(),"Dados apagados!",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("NÃO", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                //cancela o dialogo caso o utilizador deseje continuar no jogo
+                                dialog.cancel();
+                            }
+                        });
+                //Cria um caixa de dialogo
+                AlertDialog alert = builder.create();
+                //Setting the title manually
+                alert.setTitle("ALERTA");
+                alert.show();
+            }
+        });
     }
 
     public void btnConfNAME(View view){
